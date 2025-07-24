3eebcb8b36e9b8b8215fa04d40354ad43ebce799
951004031204 <a01041416062@gmail.com>
2025-07-24 11:28:07 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const songs = [
-  { title: "아주 NICE", artist: "SEVENTEEN" },
-  { title: "손오공", artist: "SEVENTEEN" },
-  { title: "HOT", artist: "SEVENTEEN" },
-  { title: "Left & Right", artist: "SEVENTEEN" },
-  { title: "Rock with you", artist: "SEVENTEEN" },
-  { title: "HOME;RUN", artist: "SEVENTEEN" },
-  { title: "Darl+ing", artist: "SEVENTEEN" },
-  { title: "Ready to love", artist: "SEVENTEEN" },
-  { title: "울고 싶지 않아", artist: "SEVENTEEN" },
-  { title: "독: Fear", artist: "SEVENTEEN" },
-  { title: "Super", artist: "SEVENTEEN" },
-  { title: "Pretty U", artist: "SEVENTEEN" },
-  { title: "Don't Wanna Cry", artist: "SEVENTEEN" },
-  { title: "Clap", artist: "SEVENTEEN" },
-  { title: "Thanks", artist: "SEVENTEEN" },
-  { title: "Shadow", artist: "SEVENTEEN" },
-  { title: "24H", artist: "SEVENTEEN" },
-  { title: "Healing", artist: "SEVENTEEN" },
-  { title: "Good to Me", artist: "SEVENTEEN" },
-  { title: "Change Up", artist: "SEVENTEEN" }
+  { title: "아주 NICE", artist: "SEVENTEEN", youtube: "tA9xN6QHk1A" },
+  { title: "손오공", artist: "SEVENTEEN", youtube: "tJw1g5h9pE8" },
+  { title: "HOT", artist: "SEVENTEEN", youtube: "r6TwzSGYycM" },
+  { title: "Left & Right", artist: "SEVENTEEN", youtube: "t7Fq1f6Q2wE" },
+  { title: "Rock with you", artist: "SEVENTEEN", youtube: "mK6x1lQ6qkY" },
+  { title: "HOME;RUN", artist: "SEVENTEEN", youtube: "Uq4gvzC1l-g" },
+  { title: "Darl+ing", artist: "SEVENTEEN", youtube: "k5w6y6l1l8A" },
+  { title: "Ready to love", artist: "SEVENTEEN", youtube: "VgVQKCcfwnU" },
+  { title: "울고 싶지 않아", artist: "SEVENTEEN", youtube: "8h2Z7lQ1Q1A" },
+  { title: "독: Fear", artist: "SEVENTEEN", youtube: "J-wFp43XOrA" },
+  { title: "Super", artist: "SEVENTEEN", youtube: "t6U2QbA4QnA" },
+  { title: "Pretty U", artist: "SEVENTEEN", youtube: "9mQk7Evt6Vw" },
+  { title: "Don't Wanna Cry", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Clap", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Thanks", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Shadow", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "24H", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Healing", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Good to Me", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" },
+  { title: "Change Up", artist: "SEVENTEEN", youtube: "t6r1QbA4QnA" }
 ];
 
 function getRandomSong() {
@@ -29,4 +29,11 @@ function getRandomSong() {
 document.getElementById('recommendBtn').addEventListener('click', () => {
   const song = getRandomSong();
   document.getElementById('songBox').innerHTML = `<strong>${song.title}</strong> <br> <span>by ${song.artist}</span>`;
+  // 유튜브 배경음악 자동 재생
+  const player = document.getElementById('musicPlayer');
+  if(song.youtube) {
+    player.innerHTML = `<iframe width="320" height="180" src="https://www.youtube.com/embed/${song.youtube}?autoplay=1&controls=0&loop=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+  } else {
+    player.innerHTML = '';
+  }
 });
